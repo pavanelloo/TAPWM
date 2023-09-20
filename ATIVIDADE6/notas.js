8d153099961f498f99c32672fb5258fad9a5c740
Victor Pavanello Amado <112418436+pavanelloo@users.noreply.github.com>
2023-09-20 15:07:31 -0300

    Add files via upload

---
+++ ATIVIDADE6/notas.js
@@ -0,0 +1,13 @@
+var nota1 
+var nota2
+var nota3
+
+nota1 = prompt("Qual sua nota?")
+nota2 = prompt("Qual sua nota?")
+nota3 = prompt("Qual sua nota?")
+
+var media = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3)) / 3
+
+alert(media)
+
+
---
+++ ATIVIDADE6/numeros.js
@@ -0,0 +1,13 @@
+var num1
+var num2
+
+num1 = prompt ("coloque um numero")
+num2 = prompt ("coloque outro numero")
+
+var soma = parseFloat(num1) + parseFloat(num2)
+var sub = num1 - num2
+var nums = "numero 1 = " + num1 +  " \n numero 2 = " + num2
+var divi = num1 / num2
+var resto = num1 % num2
+
+alert("resultado da soma =" + soma + " \n resultado subtração = " + sub + " \n resultado dos produtos = " + nums + " \n resultado da divisão = " + divi +" \n resultado do resto = " + resto)
